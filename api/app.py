1d57519a7c0c6abe7b24a96111dc896ceb9706dc
Pedro Costa <pedrocosta.lov@gmail.com>
2024-10-27 18:30:25 -0300

    cria web page

--- api/app.py
+++ api/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route('/api', methods=['GET'])
 def get_data():
     return jsonify({"message": "Hello, World!"})
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
