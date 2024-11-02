aeee1eb239d722fd20d434deae99e889ff7b7f89
Pedro Costa <pedrocosta.lov@gmail.com>
2024-11-02 19:45:50 -0300

    adiciona cors e rotas para consulta

--- api/app.py
+++ api/app.py
@@ -1,20 +1,39 @@
 from flask import Flask, jsonify, render_template, Response, request
+from flask_cors import CORS
 from datetime import datetime
 import database
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
-@app.route('/api', methods=['GET'])
+@app.route('/api/teste', methods=['GET'])
 def api_teste():
     return Response(status=200)
 
 
+@app.route('/api/ultima-leitura', methods=['GET'])
+def recuperar_ultima_leitura():
+    row = database.get_ultima_leitura()
+    
+    if row is None:
+        return jsonify({'data': []})
+    
+    data = [{'id': row['id'], 'timestamp': row['timestamp'], 'value': row['value']}]
+    
+    return jsonify({'data': data})
+
+
 @app.route('/api/recuperar-dados', methods=['GET'])
 def recuperar_dados():
-    rows = database.get_all_leituras()
+    page = request.args.get('page')
+    
+    if page:
+        rows = database.get_leituras(page=page)
+    else:
+        rows = database.get_leituras()
     
     data = [
-        { 'id': row[0], 'timestamp': row[1], 'value': row[2] }
+        {'id': row['id'], 'timestamp': row['timestamp'], 'value': row['value']}
         for row in rows
     ]
 
