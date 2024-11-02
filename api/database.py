0b1ee2508c30473181fb195220fd0c5a1e07f814
Pedro Costa <pedrocosta.lov@gmail.com>
2024-11-02 19:45:35 -0300

    adiciona funcoes de consulta

--- api/database.py
+++ api/database.py
@@ -1,27 +1,37 @@
-import sqlite3
+from sqlite_utils import Database
 
 DATABASE = 'db.sqlite'
 TABELA = 'leituras'
 
-def get_all_leituras():
-    conn = sqlite3.connect(DATABASE)
-    cursor = conn.cursor()
-    
-    cursor.execute(f"SELECT * FROM {TABELA}")
-    rows = cursor.fetchall()
-    
-    conn.close()
-    return rows
+
+def get_database_connection():
+    return Database(DATABASE)
 
 
 def salvar_dados_leitura(value, timestamp):
-    conn = sqlite3.connect(DATABASE)
-    cursor = conn.cursor()
-    
-    cursor.execute(f'''
-        INSERT INTO {TABELA} (value, timestamp)
-        VALUES (?, ?)
-    ''', (value, timestamp))
-    
-    conn.commit()
-    conn.close()
+    db = get_database_connection()
+    db[TABELA].insert({
+        "value": value,
+        "timestamp": timestamp
+    })
+
+
+def get_all_leituras():
+    db = get_database_connection()
+    rows = list(db[TABELA].rows)
+    return rows
+
+
+def get_leituras(page=1, per_page=100):
+    db = get_database_connection()
+    offset = (page - 1) * per_page
+    rows = list(db[TABELA].rows_where(
+        order_by="timestamp DESC", limit=per_page, offset=offset))
+    return rows
+
+
+def get_ultima_leitura():
+    db = get_database_connection()
+    row = next(db[TABELA].rows_where(order_by="timestamp DESC", limit=1), None)
+
+    return row
