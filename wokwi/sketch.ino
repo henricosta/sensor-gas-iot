a90ec5673d9f3e728b148aaf9eef439eb99f4689
Pedro Costa <pedrocosta.lov@gmail.com>
2024-11-02 19:37:42 -0300

    adiciona potenciometro

--- wokwi/sketch.ino
+++ wokwi/sketch.ino
@@ -3,33 +3,49 @@
 const char* ssid = "Wokwi-GUEST";
 const char* pass = "";
 
-unsigned const long interval = 2000;
-unsigned long zero = 0;
-
 void setup() {
+
   Serial.begin(115200);
+
   WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(100);
+    delay(200);
     Serial.println(".");
   }
   Serial.println("WiFi Connected!");
   Serial.println(WiFi.localIP());
 
-  HTTPClient http;
-  http.begin("https://witty-cars-give.loca.lt/api");
-  int httpResponCode = http.GET();
-  Serial.println(httpResponCode);
-  if (httpResponCode > 0) {
-    String payload = http.getString();
-    Serial.print(payload);
-  } else {
-    Serial.print("error ");
-    Serial.println(httpResponCode);
-  }
-
+  pinMode(32, INPUT);
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
+  int value = analogRead(32);
+  int intensity = map(value, 0, 4098, 0, 255);
+  analogWrite(2, intensity);
 
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+
+    String url = "https://empty-ravens-battle.loca.lt/api/salvar-dados?value=" + String(intensity);
+    http.begin(url);
+
+    int httpResponseCode = http.GET();
+
+    if (httpResponseCode > 0) {
+      String payload = http.getString();
+      Serial.print("Response: ");
+      Serial.println(payload);
+    } else {
+      Serial.print("Error: ");
+      Serial.println(httpResponseCode);
+    }
+
+    http.end();
+  } else {
+    Serial.println("WiFi disconnected");
+  }
+
+  Serial.println(intensity);
+  delay(200);
 }
