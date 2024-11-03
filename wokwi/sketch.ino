11661e2e6489db9485c98a696d544597d5b38505
Pedro Costa <pedrocosta.lov@gmail.com>
2024-11-03 11:07:41 -0300

    diminui tempo de compilacao

--- wokwi/sketch.ino
+++ wokwi/sketch.ino
@@ -1,19 +1,20 @@
 #include<WiFi.h>
 #include <HTTPClient.h>
+
 const char* ssid = "Wokwi-GUEST";
 const char* pass = "";
 
 void setup() {
 
-  Serial.begin(115200);
+  // Serial.begin(115200);
 
   WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED) {
     delay(200);
-    Serial.println(".");
+    // Serial.println(".");
   }
-  Serial.println("WiFi Connected!");
-  Serial.println(WiFi.localIP());
+  // Serial.println("WiFi Connected!");
+  // Serial.println(WiFi.localIP());
 
   pinMode(32, INPUT);
   pinMode(2, OUTPUT);
@@ -27,25 +28,25 @@ void loop() {
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
 
-    String url = "https://empty-ravens-battle.loca.lt/api/salvar-dados?value=" + String(intensity);
+    String url = "https://large-pets-work.loca.lt/api/salvar-dados?value=" + String(intensity);
     http.begin(url);
+    http.GET();
+    // int httpResponseCode = http.GET();
 
-    int httpResponseCode = http.GET();
-
-    if (httpResponseCode > 0) {
-      String payload = http.getString();
-      Serial.print("Response: ");
-      Serial.println(payload);
-    } else {
-      Serial.print("Error: ");
-      Serial.println(httpResponseCode);
-    }
+    // if (httpResponseCode > 0) {
+    //   String payload = http.getString();
+    //   Serial.print("Response: ");
+    //   Serial.println(payload);
+    // } else {
+    //   Serial.print("Error: ");
+    //   Serial.println(httpResponseCode);
+    // }
 
     http.end();
   } else {
-    Serial.println("WiFi disconnected");
+    // Serial.println("WiFi disconnected");
   }
 
-  Serial.println(intensity);
+  // Serial.println(intensity);
   delay(200);
 }
